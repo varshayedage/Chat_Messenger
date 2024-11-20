50732ebbb799f3168a4d66dbe9de32d84d12bef9
varshayedage <123730760+varshayedage@users.noreply.github.com>
2024-11-20 22:52:33 +0530

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.*;
+
+class Server
+{
+    public static void main(String Arg[]) throws Exception
+    {
+        System.out.println("Server Of Marvellous Chat messanger is Running...");
+
+        ServerSocket ssobj = new ServerSocket(2100);
+        System.out.println("Server is in the listening mode at port No :2100");
+
+        Socket sobj = ssobj.accept();
+        System.out.println("Client and Server connection is succesful....");
+
+        PrintStream ps = new PrintStream(sobj.getOutputStream());
+
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(sobj.getInputStream()));
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Marvellous Chat Messanger started...");
+
+        String str1,str2;
+
+        while((str1 = br1.readLine()) != null)
+        {
+            System.out.println("Client says : "+str1);
+            System.out.println("Enter message of client : ");
+            str2 = br2.readLine();
+            ps.println(str2);
+        }
+            System.out.println("Thank you for using the chat messanger...");
+            ssobj.close();
+            sobj.close();
+            ps.close();
+            br1.close();
+            br2.close();
+
+
+    }
+}
